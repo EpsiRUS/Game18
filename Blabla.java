ad2511d9ad4ee88aa669f7a446bf9ceba3fe4173
student2 <azute2000@gmail.com>
2016-11-25 20:29:14 +0300

    asd

--- Blabla.java
+++ Blabla.java
@@ -1,12 +1,13 @@
 package ru.samsung.itschool.game18;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-public class Blabla extends AppCompatActivity {
+public class Blabla extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
